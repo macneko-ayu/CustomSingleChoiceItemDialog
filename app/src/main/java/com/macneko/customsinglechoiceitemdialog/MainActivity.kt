ea39ca687d0178840e091184438d3233d4aa17f5
macneko-ayu <macneko.ayu@gmail.com>
2021-07-10 22:09:21 +0900

    Implemented part of the layout

--- app/src/main/java/com/macneko/customsinglechoiceitemdialog/MainActivity.kt
+++ app/src/main/java/com/macneko/customsinglechoiceitemdialog/MainActivity.kt
@@ -2,10 +2,16 @@ package com.macneko.customsinglechoiceitemdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.core.view.get
+import com.macneko.customsinglechoiceitemdialog.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+  private val binding: ActivityMainBinding by lazy {
+    ActivityMainBinding.bind(findViewById<ViewGroup>(android.R.id.content)[0])
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
   }
 }
