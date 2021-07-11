7adc34423047c131b818e18ac8aaf8e29968d11d
macneko-ayu <macneko.ayu@gmail.com>
2021-07-11 20:59:30 +0900

    Show CustomSingleChoiceItemDialogFragment

--- app/src/main/java/com/macneko/customsinglechoiceitemdialog/view/MainActivity.kt
+++ app/src/main/java/com/macneko/customsinglechoiceitemdialog/view/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
   private val binding: ActivityMainBinding by lazy {
     ActivityMainBinding.bind(findViewById<ViewGroup>(android.R.id.content)[0])
   }
+  private val disableIndex = 2
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -23,10 +24,21 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         REQUEST_KEY_NORMAL)
       dialog.show(supportFragmentManager, TAG_NORMAL)
     }
+    binding.customDialogButton.setOnClickListener {
+      val dialog = CustomSingleChoiceItemDialogFragment.newInstance(
+        getString(R.string.custom_dialog_text),
+        resources.getStringArray(R.array.dialog_values).toList(),
+        0,
+        disableIndex,
+        REQUEST_KEY_CUSTOM)
+      dialog.show(supportFragmentManager, TAG_CUSTOM)
+    }
   }
 
   companion object {
     private const val REQUEST_KEY_NORMAL = "request_key_normal"
+    private const val REQUEST_KEY_CUSTOM = "request_key_custom"
     private const val TAG_NORMAL = "NormalSingleChoiceItemDialogFragment"
+    private const val TAG_CUSTOM = "CustomSingleChoiceItemDialogFragment"
   }
 }
