8593470d60457419a76378256901a2639c841fbf
macneko-ayu <macneko.ayu@gmail.com>
2021-07-11 18:03:17 +0900

    Show NormalSingleChoiceItemDialogFragment

--- app/src/main/java/com/macneko/customsinglechoiceitemdialog/view/MainActivity.kt
+++ app/src/main/java/com/macneko/customsinglechoiceitemdialog/view/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.core.view.get
+import com.macneko.customsinglechoiceitemdialog.R
 import com.macneko.customsinglechoiceitemdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -13,5 +14,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    binding.normalDialogButton.setOnClickListener {
+      val dialog = NormalSingleChoiceItemDialogFragment.newInstance(
+        getString(R.string.normal_dialog_text),
+        resources.getStringArray(R.array.dialog_values).toList(),
+        0,
+        REQUEST_KEY_NORMAL)
+      dialog.show(supportFragmentManager, TAG_NORMAL)
+    }
+  }
+
+  companion object {
+    private const val REQUEST_KEY_NORMAL = "request_key_normal"
+    private const val TAG_NORMAL = "NormalSingleChoiceItemDialogFragment"
   }
 }
